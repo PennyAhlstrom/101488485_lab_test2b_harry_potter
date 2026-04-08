b4631c96224c5e7b59e3dd12b1dc95a6b87a83f1
Penny Ahlstrom <pennyahlstrom@hotmail.com>
2026-04-08 12:12:07 -0400

    install angular animations

--- src/app/components/character-filter/character-filter.ts
+++ src/app/components/character-filter/character-filter.ts
@@ -1,9 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-character-filter',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, MatFormFieldModule, MatSelectModule],
   templateUrl: './character-filter.html',
-  styleUrl: './character-filter.css',
+  styleUrl: './character-filter.css'
 })
-export class CharacterFilter {}
+export class CharacterFilter {
+  // House options received from parent
+  @Input() houses: string[] = [];
+
+  // Currently selected house
+  @Input() selectedHouse = 'All';
+
+  // Send selected house back to parent
+  @Output() houseChanged = new EventEmitter<string>();
+
+  onSelectionChange(value: string): void {
+    this.houseChanged.emit(value);
+  }
+}
