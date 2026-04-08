dfbb1d87115e8d136a99dd9237a3e7b48ca90ba6
Penny Ahlstrom <pennyahlstrom@hotmail.com>
2026-04-08 12:07:18 -0400

    configure routing and http client

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { CharacterDetails } from './components/character-details/character-details';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // Main page: filter + list
+  { path: '', redirectTo: 'characters', pathMatch: 'full' },
+  { path: 'characters', loadComponent: () => import('./app').then(m => m.App) },
+
+  // Details page
+  { path: 'characters/:id', component: CharacterDetails },
+
+  // Fallback
+  { path: '**', redirectTo: 'characters' }
+];
